01e086d686b9e845aadf10364e725d6f3afbc24b
jpinto <jfpinto1989@gmail.com>
2021-09-21 14:15:55 -0400

    Refactor functions as function  expressions

--- src/script.js
+++ src/script.js
@@ -1,12 +1,12 @@
 import "./style.css";
 
-function compareArrays(array1, array2) {
+let areEqualArrays = function (array1, array2) {
 	if (array1.length != array2.length) return false;
 	return array1.every((item, index) => item === array2[index]);
-}
+};
 
-function isHit(shot, positions) {
-	return positions.some((item) => compareArrays(item, shot));
-}
+let isHit = function (shot, positions) {
+	return positions.some((item) => areEqualArrays(item, shot));
+};
 
-export { compareArrays, isHit };
+export { areEqualArrays, isHit };
--- src/script.test.js
+++ src/script.test.js
@@ -1,35 +1,35 @@
 /* eslint-disable no-undef */
-import { compareArrays, isHit } from "./script.js";
+import { areEqualArrays, isHit } from "./script.js";
 
-describe("compareArrays", () => {
+describe("areEqualArrays", () => {
 	test("returns true for arrays with same elements, same order", () => {
 		let arr1 = [1, 2, 3];
 		let arr2 = [1, 2, 3];
-		expect(compareArrays(arr1, arr2)).toBe(true);
+		expect(areEqualArrays(arr1, arr2)).toBe(true);
 	});
 
 	test("returns false for arrays with same elements, diff order", () => {
 		let arr1 = [1, 2, 3];
 		let arr2 = [1, 3, 2];
-		expect(compareArrays(arr1, arr2)).toBe(false);
+		expect(areEqualArrays(arr1, arr2)).toBe(false);
 	});
 
 	test("returns false for arrays with different elements", () => {
 		let arr1 = [1, 2, 3];
 		let arr2 = [1, 2, 4];
-		expect(compareArrays(arr1, arr2)).toBe(false);
+		expect(areEqualArrays(arr1, arr2)).toBe(false);
 	});
 
 	test("returns false for arrays with same elements but diff lengths", () => {
 		let arr1 = [1, 2, 3];
 		let arr2 = [1, 2, 3, 4];
-		expect(compareArrays(arr1, arr2)).toBe(false);
+		expect(areEqualArrays(arr1, arr2)).toBe(false);
 	});
 
 	test("returns false for arrays with same elements but diff lengths", () => {
 		let arr1 = [1, 2, 3, 4];
 		let arr2 = [1, 2, 3];
-		expect(compareArrays(arr1, arr2)).toBe(false);
+		expect(areEqualArrays(arr1, arr2)).toBe(false);
 	});
 });
 
