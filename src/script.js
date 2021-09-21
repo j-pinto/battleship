6a109b1ea17bb167aaf704127e6eb80f130c6977
jpinto <jfpinto1989@gmail.com>
2021-09-21 15:59:55 -0400

    Refactor areEqualArrays() to use currying

--- src/script.js
+++ src/script.js
@@ -12,13 +12,13 @@ let curry = function (func) {
 	};
 };
 
-let areEqualArrays = function (array1, array2) {
+let areEqualArrays = curry((array1, array2) => {
 	if (array1.length != array2.length) return false;
 	return array1.every((item, index) => item === array2[index]);
-};
+});
 
 let isHit = function (shot, positions) {
-	return positions.some((item) => areEqualArrays(item, shot));
+	return positions.some(areEqualArrays(shot));
 };
 
 export { curry, areEqualArrays, isHit };
