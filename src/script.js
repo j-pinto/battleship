7880975468c31360ee3aa6dfe4b44a20e81eef5a
jpinto <jfpinto1989@gmail.com>
2021-09-21 13:25:20 -0400

    Array comparison function with tests, all pass

--- src/script.js
+++ src/script.js
@@ -1,9 +1,8 @@
 import "./style.css";
 
-console.log("hello, world!");
-
-function dummyFunc() {
-	return 1;
+function compareArrays(array1, array2) {
+	if (array1.length != array2.length) return false;
+	return array1.every((item, index) => item === array2[index]);
 }
 
-export { dummyFunc };
+export { compareArrays };
--- src/script.test.js
+++ src/script.test.js
@@ -1,5 +1,34 @@
-import { dummyFunc } from "./script.js";
+/* eslint-disable no-undef */
+import { compareArrays } from "./script.js";
 
-test("this test should pass", () => {
-	expect(dummyFunc()).toBe(1);
+describe("compareArrays", () => {
+	test("returns true for arrays with same elements, same order", () => {
+		let arr1 = [1, 2, 3];
+		let arr2 = [1, 2, 3];
+		expect(compareArrays(arr1, arr2)).toBe(true);
+	});
+
+	test("returns false for arrays with same elements, diff order", () => {
+		let arr1 = [1, 2, 3];
+		let arr2 = [1, 3, 2];
+		expect(compareArrays(arr1, arr2)).toBe(false);
+	});
+
+	test("returns false for arrays with different elements", () => {
+		let arr1 = [1, 2, 3];
+		let arr2 = [1, 2, 4];
+		expect(compareArrays(arr1, arr2)).toBe(false);
+	});
+
+	test("returns false for arrays with same elements but diff lengths", () => {
+		let arr1 = [1, 2, 3];
+		let arr2 = [1, 2, 3, 4];
+		expect(compareArrays(arr1, arr2)).toBe(false);
+	});
+
+	test("returns false for arrays with same elements but diff lengths", () => {
+		let arr1 = [1, 2, 3, 4];
+		let arr2 = [1, 2, 3];
+		expect(compareArrays(arr1, arr2)).toBe(false);
+	});
 });
